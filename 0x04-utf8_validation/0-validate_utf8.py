b49cacaa274ff43d38aa15ea5687e725a208032b
= <=>
2023-01-29 22:51:01 +0100

    validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,52 +1,33 @@
 #!/usr/bin/python3
-"""
-Define validUTF8(data) function that validates whether a
-string of ints represents a valid UTF-8 encoding.
-"""
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
+""" UTF-8 encoding validator """
 
 
 def validUTF8(data):
     """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
+    determines if a given data set represents a valid UTF-8 encoding
+
+    Argument:
+    data: list of integers
     """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
-            continue
+    count = 0
 
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
-            return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
-            return False
+    for bit in data:
+        bin_data = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
+        if count == 0:
+            if bin_data.startswith('110'):
+                count = 1
+            if bin_data.startswith('1110'):
+                count = 2
+            if bin_data.startswith('11110'):
+                count = 3
+            if bin_data.startswith('10'):
+                return False
+        else:
+            if not bin_data.startswith('10'):
+                return False
+            count -= 1
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
-                return False
-            if byte[0:2] != [1, 0]:
-                return False
+    if count != 0:
+        return False
+
     return True
